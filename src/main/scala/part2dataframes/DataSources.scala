6c1cebab355ae6bc6325009182ad6913d0b6046b
Manish Karki <mannishkarki@gmail.com>
2020-07-30 00:04:29 -0400

    used different sources to read

--- src/main/scala/part2dataframes/DataSources.scala
+++ src/main/scala/part2dataframes/DataSources.scala
@@ -59,4 +59,42 @@ object DataSources extends App {
     .save() // the option path can be directly sent in as parameter in save
 
   // Json FLags
+  spark.read
+    .schema(carsSchema)
+    .option("dateFormat", "YYYY-MM-dd")
+    .option("allowSingleQuotes", "true")
+    .option("compression", "uncompressed")
+    .json("src/main/resources/data/cars.json")
+
+  //CSV Flags
+  val stockSchema = StructType(Array(
+    StructField("symbol", StringType),
+    StructField("date", DateType),
+    StructField("price", DoubleType)
+  ))
+
+  spark.read
+    .schema(stockSchema)
+    .option("dateFormat", "MMM dd YYYY")
+    .option("header", "true") // ignores the first row
+    .option("sep", ",") // separator, can be chosen what one needs
+    .option("nullValue", "") // empty values can be used as null
+    .load("src/main/resources/data/stocks.csv") // or just csv method
+
+  // Parquet ( default storage format for DFs, open sourced compressed binary format)
+  carsDF.write
+    .mode(SaveMode.Overwrite)
+    .save("src/main/resources/data/cars") // by default since we didn't specified any format, this'll get saved as parquet
+
+  // reading from a DB postgress, just go to the terminal and enter: docker-compose up
+  val employeeDF = spark.read
+    .format("jdbc")
+    .option("driver", "org.postgresql.Driver")
+    .option("url", "jdbc:postgresql://localhost:5432/rtjvm")
+    .option("user", "docker")
+    .option("password", "docker")
+    .option("dbtable", "public.employees")
+    .load()
+  // progress: time: 19:40
+
 }
