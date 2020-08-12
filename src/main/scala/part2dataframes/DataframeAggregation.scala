92c6522a25073779a68b6cac1843ed9a353219a7
Manish Karki <mannishkarki@gmail.com>
2020-08-11 21:49:50 -0400

    aggregation count

---
+++ src/main/scala/part2dataframes/DataframeAggregation.scala
@@ -0,0 +1,23 @@
+package part2dataframes
+
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions.{count, col}
+
+/**
+  * @author mkarki
+  */
+object DataframeAggregation extends App {
+  val spark = SparkSession.builder
+    .appName("Aggregations and Grouping")
+    .config("spark.master", "local")
+    .getOrCreate
+
+  val moviesDF = spark.read.json("src/main/resources/data/movies.json")
+
+  // counting movie by genre
+  val genresCountDF = moviesDF.select(count(col("Major_genre")).as("genre_count")) // all the values except null
+  moviesDF.selectExpr("count(Major_Genre)")
+  // count all
+  moviesDF.select(count("*").as("genre_count")).show
+  genresCountDF.show
+}
