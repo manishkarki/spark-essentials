9b6f58ca8408e6aa4472bedf5e62df51a7b5927f
Manish Karki <mannishkarki@gmail.com>
2020-08-13 22:39:27 -0400

    ran the formatter

--- src/main/scala/part3typedatasets/ComplexTypes.scala
+++ src/main/scala/part3typedatasets/ComplexTypes.scala
@@ -7,7 +7,8 @@ import org.apache.spark.sql.functions._
   * @author mkarki
   */
 object ComplexTypes extends App {
-  val spark = SparkSession.builder()
+  val spark = SparkSession
+    .builder()
     .config("spark.master", "local")
     .appName("Complex types")
     .getOrCreate
@@ -20,12 +21,17 @@ object ComplexTypes extends App {
   spark.sql("set spark.sql.legacy.timeParserPolicy=LEGACY")
   //Dates
   val moviesWithReleaseDates = moviesDF
-    .select(col("Title"), to_date(col("Release_Date"), "dd-MMM-yy").as("Actual_Release")) // conversion
+    .select(col("Title"),
+            to_date(col("Release_Date"), "dd-MMM-yy")
+              .as("Actual_Release")) // conversion
     .withColumn("Today", current_date()) // today
     .withColumn("Right_now", current_timestamp()) // now
-    .withColumn("movie_age", datediff(col("Today"), col("Actual_Release")) / 365) // diff in days, similarly we also have date_add, date_sub
+    .withColumn(
+      "movie_age",
+      datediff(col("Today"), col("Actual_Release")) / 365) // diff in days, similarly we also have date_add, date_sub
 
-  moviesWithReleaseDates.select("*")
+  moviesWithReleaseDates
+    .select("*")
     .where(col("Actual_Release").isNull)
 
   /**
@@ -33,12 +39,11 @@ object ComplexTypes extends App {
     * 1. How do we deal with multiple date formats?
     * 2. Read the stocks DF and parse dates
     */
-
-    // 1 - parse the DF multiple times, then union the small DFs
-    //    - ignore the minimum rows which are corrupted
+  // 1 - parse the DF multiple times, then union the small DFs
+  //    - ignore the minimum rows which are corrupted
 
   val stocksDF = spark.read
-      .format("csv")
+    .format("csv")
     .option("header", "true")
     .load("src/main/resources/data/stocks.csv")
 
