7bc1e8571ced916fd001e2799bda2df866c34d9f
Manish Karki <mannishkarki@gmail.com>
2020-08-17 19:44:05 -0400

    reading csv source

---
+++ src/main/scala/part3typedatasets/Datasets.scala
@@ -0,0 +1,20 @@
+package part3typedatasets
+
+import org.apache.spark.sql.SparkSession
+
+/**
+  * @author mkarki
+  */
+object Datasets extends App {
+  val spark = SparkSession.builder()
+    .appName("Datasets")
+    .config("spark.master", "local")
+    .getOrCreate()
+
+  val numbersDF = spark.read
+    .format("csv")
+    .option("header", "true")
+    .load("src/main/resources/data/numbers.csv")
+
+  numbersDF.printSchema()
+}
