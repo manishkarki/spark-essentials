07285706b06810d993ec89be9f8c4221407038b8
Manish Karki <mannishkarki@gmail.com>
2020-08-17 19:30:34 -0400

    using coalese

---
+++ src/main/scala/part3typedatasets/ManagingNulls.scala
@@ -0,0 +1,25 @@
+package part3typedatasets
+
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions.{coalesce, col}
+
+/**
+  * @author mkarki
+  */
+object ManagingNulls extends App {
+  val spark = SparkSession.builder()
+    .appName("managing nulls")
+    .config("spark.master", "local")
+    .getOrCreate()
+
+  val moviesDF = spark.read
+    .json("src/main/resources/data/movies.json")
+
+  // select the first non-null value
+  moviesDF.select(
+    col("title"),
+    col("Rotten_Tomatoes_Rating"),
+    col("IMDB_Rating"),
+    coalesce(col("Rotten_Tomatoes_Rating"), col("IMDB_Rating") * 10).as("first_non_null_rating")
+  ).show()
+}
