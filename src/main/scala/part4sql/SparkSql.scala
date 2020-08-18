9b56af81e6c85ce2cdb85f9b38bfc7b68d1c69c3
Manish Karki <mannishkarki@gmail.com>
2020-08-17 21:54:52 -0400

    indenting and using SparkSql to create table and DBs in the local dir

--- src/main/scala/part4sql/SparkSql.scala
+++ src/main/scala/part4sql/SparkSql.scala
@@ -1,13 +1,14 @@
 package part4sql
 
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.{SaveMode, SparkSession}
 import org.apache.spark.sql.functions.col
 
 /**
   * @author mkarki
   */
 object SparkSql extends App {
-  val spark = SparkSession.builder()
+  val spark = SparkSession
+    .builder()
     .appName("spark sql prac")
     .config("spark.master", "local")
     .config("spark.sql.warehouse.dir", "src/main/resources/warehouse")
@@ -17,14 +18,15 @@ object SparkSql extends App {
     .json("src/main/resources/data/cars.json")
 
   // regular DF API
-  carsDF.select(col("Name"))
+  carsDF
+    .select(col("Name"))
     .where(col("Name") === "USA")
 
   // use spark sql
   carsDF.createOrReplaceTempView("cars")
 
-  spark.sql(
-    """
+  spark
+    .sql("""
       | SELECT Name FROM cars WHERE origin = 'USA'
     """.stripMargin)
     .show()
@@ -33,4 +35,48 @@ object SparkSql extends App {
   spark.sql("CREATE database rtjvm")
   spark.sql("USE rtjvm")
   val databasesDF = spark.sql("SHOW databases")
+
+  // transfer tables from a DB to spark tables
+  val driver: String = "org.postgresql.Driver"
+  val url: String = "jdbc:postgresql://localhost:5432/rtjvm"
+  val uName: String = "docker"
+  val password: String = "docker"
+
+  // reading from a DB postgress, just go to the terminal and enter: docker-compose up
+  def readTable(tableName: String) =
+    spark.read
+      .format("jdbc")
+      .option("driver", driver)
+      .option("url", url)
+      .option("user", uName)
+      .option("password", password)
+      .option("dbtable", s"public.$tableName")
+      .load()
+
+  def transferTables(tableNames: List[String]) =
+    tableNames.foreach(tableName => {
+      val tableDF = readTable(tableName)
+      tableDF.createOrReplaceTempView(tableName)
+      tableDF.write
+        .mode(SaveMode.Overwrite)
+        .saveAsTable(tableName)
+    })
+  val employeesDF = readTable("employees")
+  employeesDF.write
+    .mode(SaveMode.Overwrite)
+    .saveAsTable("employees")
+
+  transferTables(
+    List("employees",
+         "departments",
+         "titles",
+         "dept_emp",
+         "salaries",
+         "dept_manager")
+  )
+
+  // read DF from DW
+  val empployeesDF2 = spark.read
+    .table("employees")
+    .show()
 }
