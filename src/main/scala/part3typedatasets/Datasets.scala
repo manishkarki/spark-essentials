be9539088c4683cbd35acc432a40028d1fbc9966
Manish Karki <mannishkarki@gmail.com>
2020-08-17 20:45:08 -0400

    ds joins

--- src/main/scala/part3typedatasets/Datasets.scala
+++ src/main/scala/part3typedatasets/Datasets.scala
@@ -9,7 +9,8 @@ import org.apache.spark.sql.{DataFrame, Dataset, Encoders, SparkSession}
   * @author mkarki
   */
 object Datasets extends App {
-  val spark = SparkSession.builder()
+  val spark = SparkSession
+    .builder()
     .appName("Datasets")
     .config("spark.master", "local")
     .getOrCreate()
@@ -27,24 +28,26 @@ object Datasets extends App {
   // dataset of a complex type
   // 1 - define the case class
   case class Car(
-                Name: String,
-                Miles_per_Gallon: Option[Double],
-                Cylinders: Long,
-                Displacement: Double,
-                HorsePower: Option[Long],
-                Weight_in_lbs: Long,
-                Acceleration: Double,
-                Year: Date,
-                Origin: String
-                )
+      Name: String,
+      Miles_per_Gallon: Option[Double],
+      Cylinders: Long,
+      Displacement: Double,
+      HorsePower: Option[Long],
+      Weight_in_lbs: Long,
+      Acceleration: Double,
+      Year: Date,
+      Origin: String
+  )
   // 2 - read the DF from the file
-  def readDF(fileName: String) = spark.read
-    .option("inferSchema", "true")
-    .json(s"src/main/resources/data/$fileName")
+  def readDF(fileName: String) =
+    spark.read
+      .option("inferSchema", "true")
+      .json(s"src/main/resources/data/$fileName")
 
   // 3 - define an encoder
   import spark.implicits._
-  val carsDF = readDF("cars.json").withColumn("Year", to_date($"Year", "yyyy-MM-dd"))
+  val carsDF =
+    readDF("cars.json").withColumn("Year", to_date($"Year", "yyyy-MM-dd"))
 //  implicit val carEncoder = Encoders.product[Car]
   // 4 - convert DF to DS
   val carsDS = carsDF.as[Car]
@@ -62,7 +65,6 @@ object Datasets extends App {
     * 2. Powerful cars count, HP > 140
     * 3. Compute the avg HP of entire DS
     */
-
   //1
   val carsCount = carsDS.count()
   println(carsDS.count())
@@ -74,6 +76,24 @@ object Datasets extends App {
   println(carsDS.map(_.HorsePower.getOrElse(0L)).reduce(_ + _) / carsCount)
 
   // also use DF functions
-  carsDS.select(avg("HorsePower")).show()
+  carsDS.select(avg("HorsePower"))
+
+  // Joins
+  case class Guitar(id: Long, make: String, model: String, guitarType: String)
+  case class GuitarPlayer(id: Long,
+                          name: String,
+                          guitars: Seq[Long],
+                          band: Long)
+  case class Band(id: Long, name: String, homeTown: String, year: Long)
+
+  val guitarsDS = readDF("guitars.json").as[Guitar]
+  val guitarPlayersDS = readDF("guitarPlayers.json").as[GuitarPlayer]
+  val bandDS = readDF("bands.json").as[Band]
+
+  val guitarPlayerBandsDS: Dataset[(GuitarPlayer, Band)] =
+    guitarPlayersDS.joinWith(bandDS,
+                             guitarPlayersDS.col("band") === bandDS.col("id"),
+                             "inner") // default is inner
+  guitarPlayerBandsDS.show()
 
 }
