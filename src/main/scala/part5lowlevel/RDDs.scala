94acb904d91ba00ad490fda2232492e8393172d1
Manish Karki <mannishkarki@gmail.com>
2020-08-18 20:34:23 -0400

    different ways of creating RDDs create DF, DS from RDDs

--- src/main/scala/part5lowlevel/RDDs.scala
+++ src/main/scala/part5lowlevel/RDDs.scala
@@ -2,6 +2,8 @@ package part5lowlevel
 
 import org.apache.spark.sql.SparkSession
 
+import scala.io.Source
+
 /**
   * @author mkarki
   */
@@ -11,5 +13,44 @@ object RDDs extends App {
     .config("spark.master", "local")
     .getOrCreate
 
+  // entry point to rdds
+  val sc = spark.sparkContext
 
+  // 1 - parallelize an existing collection
+  val numbers = 1 to 1000000
+  val numbersRDD = sc.parallelize(numbers)
+
+  // 2 - reading from Files
+  case class StockValue(company: String, date: String, price: Double)
+
+  def readStocks(fileName: String) =
+    Source.fromFile(fileName)
+      .getLines()
+      .drop(1)
+      .map(line => line.split(","))
+      .map(tokens => StockValue(tokens(0), tokens(1), tokens(2).toDouble))
+      .toList
+
+  val stocksRDD = sc.parallelize(readStocks("src/main/resources/data/stocks.csv"))
+
+  // 2b - reading from files
+  val stocksRDD2 = sc.textFile("src/main/resources/data/stocks.csv")
+    .map(line => line.split(","))
+    .filter(tokens => tokens(0).toUpperCase() == tokens(0))
+    .map(tokens => StockValue(tokens(0), tokens(1), tokens(2).toDouble))
+
+  // 3 - read from DF
+  val stocksDF = spark.read
+    .option("header", "true")
+    .csv("src/main/resources/data/stocks.csv")
+
+  import spark.implicits._
+  val stocksDS = stocksDF.as[StockValue]
+  val stocksRDD3 = stocksDS.rdd
+
+  // RDD -> DF
+  val numbersDF = numbersRDD.toDF("numbers")
+
+  // RDD -> DS
+  val numbersDS = spark.createDataset(numbersRDD)
 }
